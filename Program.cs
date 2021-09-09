e890579d41a51cb637a34cfe5bc56a40e34db1e4
Tony Bapuji <tony_bapuji@yahoo.co.uk>
2021-09-09 22:09:20 +0100

    First working version. Reduced the minutes array to 31 members from 59. Simplified GetSpokenSpeechPart. Removed and renamed string variables in Time to class level to make available in other functions.

--- Program.cs
+++ Program.cs
@@ -7,19 +7,25 @@ namespace TalkingClockCodingChallenge
     {
         static void Main(string[] args)
         {
-
+            string instruction = "Please use format h:mm or hh:mm, where h is hour and m is minute.";
             Console.WriteLine("Talking Clock");
             Console.WriteLine("To get current time simply hit return.");
-            Console.WriteLine();
             Console.WriteLine("To get user-defined time enter time and  hit return.");
-            Console.WriteLine("(please use format h:mm or hh:mm, where h is hour and m is minute)");
-            while (true)
+            Console.WriteLine($"{instruction}");
+            while(true)
             {
-                string? t1 = Console.ReadLine();
-                Console.WriteLine(TalkingClock.Time(t1));
-                //if("Q" == Console.ReadLine()) break;
+                try
+                {
+                    string? t1 = Console.ReadLine();
+                    Console.WriteLine(TalkingClock.Time(t1));
+                    //if("Q" == Console.ReadLine()) break;}
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"{instruction}");
+                }
+                //Console.ReadLine();
             }
-            //Console.ReadLine();
         }
     }
 }
--- TalkingClock.cs
+++ TalkingClock.cs
@@ -9,55 +9,54 @@ namespace TalkingClockCodingChallenge
 {
     public class TalkingClock
     {
-        //private string _time = String.Empty;
-        private static string[] hours = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve" };
-        //TODO:  Could refactor and remove around half of these over thirty
-        private static string[] minutes = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
-        "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", "twenty four",
-        "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine", "thirty", "thirty one","thirty two","thirty three","thirty four", "thirty five", "thirty six",
-        "thirty seven", "thirty eight", "thirty nine", "forty", "forty one","forty two","forty three", "forty four", "quarter", "forty six", "forty seven",
-        "forty eight", "forty nine", "fifty", "fifty one", "fifty two", "fifty three", "fifty four", "fifty five", "fifty six", "fifty seven", "fifty eight", "fifty nine"};
-        //public TalkingClock()
-        //{
-        //}
+        private static string hoursText = String.Empty;
+        private static string minutesText = String.Empty;
+        private static string spokenSpeechPart = String.Empty;
 
+        private static readonly string[] hours = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve" };
+        //TODO:  Could refactor and remove around half of these over thirty
+        private static readonly string[] minutes = new string[] { "o'clock", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+        "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", "twenty four",
+        "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine","half" };
+        
         public static string Time(string time)
         {
             DateTime dateTime;
             if(CorrectFormat(time, out dateTime))
             {
-                if(MoreThanHalfPast(dateTime.Minute)) { dateTime.AddHours(1); } // e.g. 14:35 is twenty five to THREE (not TWO)
-                string hours = GetSpokenHours(dateTime.Hour);
-                string minutes = GetSpokenMinutes(dateTime.Minute);
-                string spokenSpeechPart = GetSpokenSpeechPart(dateTime);
-                return GetSpokenTime(dateTime);
+                if(MoreThanHalfPast(dateTime.Minute)) { dateTime = dateTime.AddHours(1); } // e.g. 14:35 is twenty five to THREE (not TWO)
+                hoursText = GetSpokenHours(dateTime.Hour);
+                minutesText = GetSpokenMinutes(dateTime.Minute);
+                spokenSpeechPart = GetSpokenSpeechPart(dateTime);
+                return CapitaliseFirstLetter(GetSpokenTime(dateTime));
             }
             else 
-            { 
-                return "faile"; 
+            {
+                throw new FormatException($"Incorrect format in {dateTime}.");
             }
         }
         private static bool CorrectFormat(string time, out DateTime dateTime)
         {
-            //This checks if the time entered is valid
+            //This checks if the time entered is in the required format
+            //TODO: refactor e.g store formats separately and have a new parameter string[] timeFormats
             string formatSingleHourDigit = "H:mm";    //24 hour formats
             string formatDoubleHourDigit = "HH:mm";
-            //DateTime dateTime;
-
+            
             return (DateTime.TryParseExact(time, formatSingleHourDigit, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime)
                    || DateTime.TryParseExact(time, formatDoubleHourDigit, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime));
         }
         private static string GetSpokenHours(int hourNumber)
         { 
-            if (hourNumber >12) { hourNumber -= 12; }   // since e.g. 14:00 is two o'clock and not fourteen o'clock
+            if (hourNumber > 12) { hourNumber -= 12; }   // since e.g. 14:00 is two o'clock and not fourteen o'clock
+            else if (hourNumber == 0) { hourNumber = 12; }
             for(int i=0;i<hours.Length;i++)
             {
                 if(hourNumber-1==i)
                 return hours[i];
 
             }
+            
             throw new Exception("Spoken hours not found");
-            //return "";
         }
         private static bool MoreThanHalfPast(int minuteNumber)
         {
@@ -69,7 +68,7 @@ namespace TalkingClockCodingChallenge
             if(minuteNumber > 30) { minuteNumber = 60 - minuteNumber; } // since e.g. 14:00 is two o'clock and not fourteen o'clock
             for(int i = 0; i < minutes.Length; i++)
             {
-                if(minuteNumber - 1 == i)
+                if(minuteNumber == i)
                 // could have the function as bool and set value to private property...
                     return minutes[i];
 
@@ -81,20 +80,26 @@ namespace TalkingClockCodingChallenge
         {
             string spokenSpeechPart = String.Empty;
             if(dateTime.Minute == 00) { spokenSpeechPart = "o'clock"; }
-            else if(dateTime.Minute > 00 && dateTime.Minute < 30) { spokenSpeechPart = "past"; }
-            else if(dateTime.Minute == 30) { spokenSpeechPart = "half past"; }
-            else if(dateTime.Minute > 30 && dateTime.Minute < 60) { spokenSpeechPart = "to"; }
+            else if(dateTime.Minute > 00 && dateTime.Minute <= 30) { spokenSpeechPart = "past"; }
+            //else if(dateTime.Minute == 30) { spokenSpeechPart = "half past"; }
+            else if(dateTime.Minute > 30) { spokenSpeechPart = "to"; }
             return spokenSpeechPart;
         }
 
         private static string GetSpokenTime(DateTime dateTime)
         {
-            return $"";
+            string spokenTime = String.Empty;
+
+            if(spokenSpeechPart == "o'clock") { spokenTime = $"{hoursText} {spokenSpeechPart}"; }
+            else if(spokenSpeechPart == "past") { spokenTime = $"{minutesText} {spokenSpeechPart} {hoursText}"; }
+            else if(spokenSpeechPart == "to") { spokenTime = $"{minutesText} {spokenSpeechPart} {hoursText}"; }
+            return spokenTime;
         }
-        private static string CapitaliseFirstLetter(string time)
+        private static string CapitaliseFirstLetter(string text)
         {
-            
-            return time;
+            if(String.IsNullOrEmpty(text)) throw new ArgumentException();
+            return text.First().ToString().ToUpper() + text.Substring(1);
+
         }
     }
 }
